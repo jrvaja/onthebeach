85f1d740cd5670b00ba1ed83d29987717dce242b
Thor <thor@Thors-MacBook-Pro.local>
2019-06-29 18:41:51 +0530

    refactoring and optimisation

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const getSequence = () => {
   const inputTextBox = document.getElementById('inputTextBox').value.trim();
   const sequenceManipulation = new SequenceManipulation(inputTextBox)
-  sequenceManipulation.make();
+  const result = sequenceManipulation.make();
+  document.getElementById('result').innerHTML = result;
 }
 
 class SequenceManipulation {
@@ -10,12 +11,17 @@ class SequenceManipulation {
     this.formatedData = this.prepareDataStructure(textInput);
   }
 
+  /**
+   * prepare data structure in dictionary form
+   * @param {String} textInput 
+   */
   prepareDataStructure(textInput) {
     const rec = textInput.match(/[^\r\n]+/g);
     return rec.map(item => {
       let element = item.split('=>');
+      
       let key = element[0].trim()
-      let value = element[1].trim() !== "" ? element[1].trim() : null
+      let value = element.length > 1 && element[1].trim() !== "" ? element[1].trim() : null
       return {
         key,
         value
@@ -23,20 +29,22 @@ class SequenceManipulation {
     });
   }
 
+  /**
+   * Prepare sequnce 
+   * @returns {String}
+   */
   make() {
-    const res = this.prepareSequence();
-    console.log(res);
-  }
-
-  prepareSequence() {
     const result = [];
     const args = this.formatedData;
     try {
       for (let item of args) {
-        if (item.value === null && !result.includes(item.key)) {
-          result.push(item.key);
-        } else {
-          this.checkDependancy(item, result);
+        if (!result.includes(item.key)) {
+          if (item.value === null) {
+            result.push(item.key);
+          } else {
+            console.log(item)
+            this.checkDependancy(item, result);
+          }
         }
       }
       return result.join('');
@@ -45,31 +53,47 @@ class SequenceManipulation {
     }
   }
 
+  /**
+   * getItemObjectByKey: Get specific item by key
+   * @param {String} key 
+   */
   getItemObjectByKey(key) {
     const data = this.formatedData;
     return data.find(item => item.key === key)
   }
 
+  /**
+   * validate key & value
+   * @param {Object} arg 
+   */
   checkValidKeyValueCombination(arg) {
-    const data = this.formatedData;
-    // check the item.value is used as key in args {throw}
-    if (data.findIndex(item => item.key === arg.value) === -1) {
-      throw `Key: ${arg.value} does not exists`;
+    if (arg.value !== null) {
+      const data = this.formatedData;
+      // check the item.value is used as key in args {throw}
+      if (data.findIndex(item => item.key === arg.value) === -1) {
+        throw `Key: ${arg.value} does not exists`;
+      }
+      // key === value {throw}
+      if (arg.key === arg.value) {
+        throw "The result should be an error stating that jobs can’t depend on themselves.";
+      }
+      return true;
     }
-    // key === value {throw}
-    if (arg.key === arg.value) {
-      throw "The result should be an error stating that jobs can’t depend on themselves.";
-    }
+    return;
   }
 
+  /**
+   * 
+   * @param {Object} currentItem 
+   * @param {Array} result 
+   * @param {Array} runtimeStack 
+   */
   checkDependancy(currentItem, result, runtimeStack = []) {
-    if (currentItem.value !== null) {
-      this.checkValidKeyValueCombination(currentItem);
-    }
+    const isValidDependcany = this.checkValidKeyValueCombination(currentItem);
     if (!result.includes(currentItem.key)) {
       runtimeStack.push(currentItem.key);
     }
-    if (currentItem.value !== null) {
+    if (isValidDependcany) {
       if (runtimeStack.includes(currentItem.value)) {
         throw "The result should be an error stating that jobs can’t have circular dependencies.";
       }
